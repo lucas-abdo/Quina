c3fb2c0bf9462894a263420e97f2fb6d53478bdf
Lucas Abdo <lucas.abdo@dtidigital.com.br>
2021-02-13 22:51:11 -0300

    Permitindo usuários inserirem as peças no tabuleiro

--- cliente/fonte/cliente.js
+++ cliente/fonte/cliente.js
@@ -17,12 +17,80 @@ const aoEnviarMensagem = (sock) => (e) => {
     sock.emit('mensagem', texto);
 };
 
+const obterCoordenadasDoMouse = (elemento, evento) => {
+    const { top, left } = elemento.getBoundingClientRect();
+    const { clientX, clientY } = evento;
+
+    return {
+        x: clientX - left,
+        y: clientY - top
+    };
+};
+
+const obterTabuleiro = (tela, numCelulas = 18) => {
+    const ctx = tela.getContext('2d');
+    const tamanhoCelula = Math.floor(tela.width / numCelulas);
+
+    const preencherCirculo = (x, y, cor) => {
+        ctx.beginPath();
+        ctx.arc(x * tamanhoCelula, y * tamanhoCelula, tamanhoCelula / 2, 0, 2 * Math.PI);
+        ctx.fillStyle = cor;
+        ctx.fill();
+    };
+
+    const desenharGrade = () => {
+        ctx.beginPath();
+        for (let i = 0; i < numCelulas + 1; i++) {
+            ctx.moveTo(i * tamanhoCelula, 0);
+            ctx.lineTo(i * tamanhoCelula, numCelulas * tamanhoCelula);
+
+            ctx.moveTo(0, i * tamanhoCelula);
+            ctx.lineTo(numCelulas * tamanhoCelula, i * tamanhoCelula);
+        }
+        ctx.stroke();
+    };
+
+    const limpar = () => {
+        ctx.clearRect(0, 0, tela.width, tela.height);
+    };
+
+    const reiniciar = () => {
+        limpar();
+        desenharGrade();
+    };
+
+    const obterCoordenadasIntersecao = (x, y) => {
+        return {
+            x: Math.floor((x + tamanhoCelula / 2) / tamanhoCelula),
+            y: Math.floor((y + tamanhoCelula / 2) / tamanhoCelula)
+        };
+    };
+
+    return { preencherCirculo, reiniciar, obterCoordenadasIntersecao };
+};
+
 (() => {
+    const tela = document.querySelector('canvas');
+    const { preencherCirculo, reiniciar, obterCoordenadasIntersecao } = obterTabuleiro(tela);
     const sock = io();
 
+    const aoClicar = (e) => {
+        const { x, y } = obterCoordenadasDoMouse(tela, e);
+        sock.emit('turno', obterCoordenadasIntersecao(x, y));
+    };
+
+    const aoMover = (e) => {
+        const { x, y } = obterCoordenadasDoMouse(tela, e);
+    };
+
+    sock.on('tabuleiro', reiniciar);
     sock.on('mensagem', log);
+    sock.on('turno', ({ x, y, cor }) => preencherCirculo(x, y, cor));
 
     document
         .querySelector('#chat-form')
         .addEventListener('submit', aoEnviarMensagem(sock));
+
+    tela.addEventListener('click', aoClicar);
+    tela.addEventListener('mousemove', aoMover);
 })();
--- servidor/servidor.js
+++ servidor/servidor.js
@@ -8,8 +8,14 @@ app.use(express.static(`${__dirname}/../cliente`));
 const servidor = http.createServer(app);
 const io = socketio(servidor);
 
+let entrada = 0;
+
 io.on('connection', (sock) => {
+    ++entrada;
+    const cor = entrada == 1 ? 'red' : (entrada == 2 ? 'yellow' : 'black');
     sock.on('mensagem', (texto) => io.emit('mensagem', texto));
+    sock.on('turno', ({ x, y }) => io.emit('turno', { x, y, cor }));
+    io.emit('tabuleiro');
 });
 
 servidor.on('error', (erro) => console.log(erro));
